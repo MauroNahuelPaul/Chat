5fb52e46bd372c0e30bb69f29f0705f83f943ea6
MauroNahuelPaul <nahuelpaul2002@gmail.com>
2023-07-07 23:55:20 -0300

    Se agrego Nodemon

--- src/app.js
+++ src/app.js
@@ -8,49 +8,48 @@ import chatRouter from "./routers/chat.router.js"
 import viewsProductsRouter from "./routers/views.router.js";
 
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+(async () => {
+    try {
+        await mongoose.connect('mongodb+srv://mauro:mauro@ecommerce.wnnj4ej.mongodb.net/?retryWrites=true&w=majority')
+        const serverHttp = app.listen(8080, () => console.log("Server Up"))
+        const io = new Server(serverHttp)
+        app.set("socketio", io);
 
+        app.engine("handlebars", handlebars.engine());
+        app.set("views", "./src/views");
+        app.set("view engine", "handlebars");
 
-try {
-    await mongoose.connect('mongodb+srv://mauro:mauro@ecommerce.wnnj4ej.mongodb.net/?retryWrites=true&w=majority')
-    const serverHttp = app.listen(8080, () => console.log("Server Up"))
-    const io = new Server(serverHttp)
-    app.set("socketio", io);
-
-    app.engine("handlebars", handlebars.engine());
-    app.set("views", "./src/views");
-    app.set("view engine", "handlebars");
-
-    app.use("/", viewsProductsRouter);
-    app.use("/api/chat", chatRouter);
-    app.use("/api/products", productRouter);
-
+        app.use("/", viewsProductsRouter);
+        app.use("/api/chat", chatRouter);
+        app.use("/api/products", productRouter);
 
 
-    io.on('connection', async socket => {
-        //Chat
-        const resultChat = await fetch("http://localhost:8080/api/chat")
-        const dataChat = await resultChat.json()
-        io.emit("updatedChat",dataChat.payload);
-        socket.on("updatedChat", async () => {
+
+        io.on('connection', async socket => {
+            //Chat
             const resultChat = await fetch("http://localhost:8080/api/chat")
             const dataChat = await resultChat.json()
             io.emit("updatedChat", dataChat.payload);
-        });
-        //Products
-        const resultProducts = await fetch("http://localhost:8080/api/products")
-        const dataProducts = await resultProducts.json()
-        io.emit("updatedProducts",dataProducts.payload);
-        socket.on("productList", async () => {
+            socket.on("updatedChat", async () => {
+                const resultChat = await fetch("http://localhost:8080/api/chat")
+                const dataChat = await resultChat.json()
+                io.emit("updatedChat", dataChat.payload);
+            });
+            //Products
             const resultProducts = await fetch("http://localhost:8080/api/products")
             const dataProducts = await resultProducts.json()
             io.emit("updatedProducts", dataProducts.payload);
-        });
-    })
-} catch (err) {
-    console.log(err)
-}
-
+            socket.on("productList", async () => {
+                const resultProducts = await fetch("http://localhost:8080/api/products")
+                const dataProducts = await resultProducts.json()
+                io.emit("updatedProducts", dataProducts.payload);
+            });
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})()
 
